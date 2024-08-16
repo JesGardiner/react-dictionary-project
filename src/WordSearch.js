68df13e277dcf031ac22944b974098b9b1612142
JesGardiner <jes.gardiner21@gmail.com>
2024-08-16 15:52:22 +0100

    Added definitions component

--- src/WordSearch.js
+++ src/WordSearch.js
@@ -1,18 +1,20 @@
 import React, {useState} from  "react";
 import axios from "axios";
+import Results from "./Results";
 
 export default function WordSearch() {
    let [keyword, setKeyword] = useState(""); 
-    
+    let [results, setResults] = useState({});
+
    function handleResponse(response) {
     console.log(response.data[0]);
-   }
+    setResults(response.data[0]);
+}
 
    function search(event) {
         event.preventDefault();
 
-        let apiKey = "171b7dtao42cf84308ef17ccf77309a5";
-        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
     }
 
@@ -25,6 +27,7 @@ export default function WordSearch() {
             <form onSubmit={search}>
                 <input type="search" onChange={handleKeywordChange} />
             </form>
+            <Results results={results} />
         </div>
     )
 }
